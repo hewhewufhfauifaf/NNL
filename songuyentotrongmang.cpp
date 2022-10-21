b1485b5fb7d6b14c05e0f1048af5bbf5b9d6a3b3
nguyenlonglha <116350704+hewhewufhfauifaf@users.noreply.github.com>
2022-10-21 21:05:45 +0700

    Add files via upload

---
+++ songuyentotrongmang.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int check_so_nguyen_to(int n){
+    for(int i=2;i<=sqrt(n);i++){
+         if(n%i==0) return 0;
+    }
+    return n>1;
+}
+int main(){
+    cout<<"chuong trinh tim so nguyen to trong 1 mang"<<endl;
+    cout<<"so phan tu cua mang la :";
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cout<<"phan tu thu "<<i<<"la :";
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(check_so_nguyen_to(a[i])){
+            cout<<"cac phan tu trong mang la so nguyen to la :";
+            cout<<a[i]<<endl;
+        }
+    }
+}
